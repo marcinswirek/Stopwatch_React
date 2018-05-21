6f1206cede498b2242c4e8b70c6abad44a6fb118
Marcin Świrek <swirek.marcin@gmail.com>
2018-05-21 11:46:38 +0200

    Add necessary file and folders

---
+++ script.js
@@ -0,0 +1,12 @@
+class StopWatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            running = false,
+            display = display,
+        }
+    }
+}
+
+var element = React.createElement(StopWatch);
+ReactDOM.render(element, document.getElementById('app'));
